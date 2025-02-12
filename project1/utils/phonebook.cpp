a86597728fda0344709d17ede3d79530ca995268
Abi Liu <abiliu018@gmail.com>
2025-02-12 10:53:04 -0800

    implemented addContact method for the phonebook

--- project1/utils/phonebook.cpp
+++ project1/utils/phonebook.cpp
@@ -3,7 +3,37 @@
 //
 
 #include <iostream>
+#include <map>
 #include "phonebook.h"
 #include "person.h"
 using namespace PersonNamespace;
 using namespace std;
+
+// later we can modify this function to load a list of contacts from a text file
+map<string, Person> createPhonebook() {
+  map<string, Person> phonebook;
+  return phonebook;
+}
+
+
+void printContacts(){}
+
+// for later projects I will add input validation for phoneNumbers
+bool addContact(map<string, Person>& phonebook, const string& phoneNumber, Person& person){
+  // first we search to see if the phone number already exists
+  if(phonebook.find(phoneNumber) == phonebook.end()){
+    // phonenumber does not exist, add and return true
+    phonebook[phoneNumber] = person;
+    return true;
+  }
+
+  // this phone number already exists, do not modify the map and return false;
+  return false;
+}
+
+bool deleteContact(){}
+bool updateContact(){}
+
+Person search(const string& phoneNumber){
+
+}
--- project1/utils/phonebook.h
+++ project1/utils/phonebook.h
@@ -5,10 +5,16 @@
 #ifndef PHONEBOOK_H
 #define PHONEBOOK_H
 
+#include <map>
+#include <iostream>
+#include "person.h"
+
+std::map<std::string, PersonNamespace::Person> createPhonebook();
 void printContacts();
-bool addContact();
+bool addContact(std::map<std::string, PersonNamespace::Person>& phonebook, const std::string& phoneNumber, PersonNamespace::Person& person);
 bool deleteContact();
 bool updateContact();
+PersonNamespace::Person search(const std::string& phoneNumber);
 
 
 #endif //PHONEBOOK_H
