1dc0d13fd89a88ca93a57279da9fa7f7a263ccdc
Abi Liu <abiliu018@gmail.com>
2025-04-29 14:04:10 -0700

    test for search by name and number

--- project1/tests/phonebook_test.cpp
+++ project1/tests/phonebook_test.cpp
@@ -1,6 +1,8 @@
 #include <gtest/gtest.h>
 #include "../utils/phonebook.h"
 #include "../utils/person.h"
+#include <vector>
+using namespace std;
 using namespace PersonNamespace;
 using namespace PhonebookNamespace;
 
@@ -69,4 +71,39 @@ TEST(PhonebookTest, updateContactFail) {
     Person update("John Doe", "1234567890");
     bool res = phonebook.updateContact(p1, update);
     EXPECT_FALSE(res);
+}
+
+TEST(PhonebookTest, searchByNumberSuccess) {
+    Phonebook phonebook;
+    Person p1("John", "1234567890");
+    phonebook.addContact(p1);
+    Person* res = phonebook.searchByNumber("1234567890");
+    EXPECT_NE(res, nullptr);
+    EXPECT_EQ(res->name, "John");
+    EXPECT_EQ(res->phoneNumber, "1234567890");
+}
+
+// person pointer should be null
+TEST(PhonebookTest, searchByNumberFail) {
+    Phonebook phonebook;
+    Person p1("John", "1234567890");
+    phonebook.addContact(p1);
+    Person* res = phonebook.searchByNumber("9234128560");
+    EXPECT_EQ(res, nullptr);
+}
+
+TEST(PhonebookTest, searchByName) {
+    Phonebook phonebook;
+    Person p1("John", "1234567890");
+    Person p2("John Doe", "0987654321");
+    Person p3("Alice Doe", "1111111111");
+    phonebook.addContact(p1);
+    phonebook.addContact(p2);
+    phonebook.addContact(p3);
+    vector<Person> res = phonebook.searchByName("John");
+    EXPECT_EQ(res.size(), 2);
+
+    // case where name not found
+    res = phonebook.searchByName("asdf");
+    EXPECT_EQ(res.size(), 0);
 }
