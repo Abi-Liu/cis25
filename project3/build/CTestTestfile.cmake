fb65ab2d4e7c20991d40c2f95dbdebc074c00c73
Abi Liu <abiliu018@gmail.com>
2025-05-24 18:39:47 -0700

    added build to gitignore

--- project3/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/abiliu/workspaces/bcc/cis25/activities/project3
-# Build directory: /Users/abiliu/workspaces/bcc/cis25/activities/project3/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/Users/abiliu/workspaces/bcc/cis25/activities/project3/build/tests[1]_include.cmake")
-subdirs("_deps/googletest-build")
